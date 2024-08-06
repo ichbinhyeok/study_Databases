42cba111e3d301a03dad295f85e5a7ed09772178
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-06 11:06:11 +0900

    UPDATE VALUES

--- sqls/db_car/CAR_COMPANY_DELETE.sql
+++ sqls/db_car/CAR_COMPANY_DELETE.sql
@@ -1,8 +1,8 @@
--- YEAR 테이블에서 car_pk = 'car003'인 레코드 삭제
-DELETE FROM `YEAR` WHERE `CAR_PK` = 'car003';
+-- 먼저 OPTION 테이블에서 CAR_PK01와 연결된 레코드를 삭제
+DELETE FROM `OPTION` WHERE `CAR_PK` = 'CAR_PK01';
 
--- OPTION 테이블에서 car_pk = 'car003'인 레코드 삭제
-DELETE FROM `OPTION` WHERE `CAR_PK` = 'car003';
+-- 그런 다음 YEAR 테이블에서 CAR_PK01와 연결된 레코드를 삭제
+DELETE FROM `YEAR` WHERE `CAR_PK` = 'CAR_PK01';
 
--- CAR 테이블에서 car_pk = 'car003'인 레코드 삭제
-DELETE FROM `CAR` WHERE `CAR_PK` = 'car003';
+-- 마지막으로 CAR 테이블에서 CAR_PK01 레코드를 삭제
+DELETE FROM `CAR` WHERE `CAR_PK` = 'CAR_PK01';
