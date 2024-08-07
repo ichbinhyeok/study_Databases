7ca78b6c5634750705a2bd07d3d5bd47989e2472
shinhyeok22 <shinhyeok22@gmail.com>
2024-08-07 11:47:25 +0900

    Orders_outjoin.sql Orders_count_toplist.sql

--- sqls/study/Orders_count_toplist.sql
+++ sqls/study/Orders_count_toplist.sql
@@ -1,4 +1,4 @@
-SELECT Employees.EmployeeID, Employees.LastName, Employees.FirstName, COUNT(Orders.OrderID) AS "PERFORMANCE"
+SELECT Employees.EmployeeID, Employees.LastName, Employees.FirstName, COUNT(Orders.OrderID) AS "참 잘했어요"
 FROM Employees
          LEFT JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
 GROUP BY Employees.EmployeeID, Employees.LastName, Employees.FirstName
